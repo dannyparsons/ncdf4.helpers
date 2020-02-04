79f196b4e384a1f8f197ec4eb331cca5f628204c
Danny Parsons <danny@aims.ac.za>
2020-02-04 13:31:37 +0300

    improved detecting of dimensions in nc.get.dim.axes.from.names()

--- R/helpers.r
+++ R/helpers.r
@@ -543,7 +543,7 @@ nc.get.dim.names <- function(f, v) {
 nc.get.dim.axes.from.names <- function(f, v, dim.names) {
   if(missing(dim.names))
     dim.names <- nc.get.dim.names(f, v)
-  return(sapply(dim.names, function(x, y) { ifelse(any(x == names(y)), y[x == names(y)], NA) }, c("lat"="Y", "latitude"="Y", "lon"="X", "longitude"="X", "xc"="X", "yc"="Y", "x"="X", "y"="Y", "time"="T", "timeofyear"="T", "plev"="Z", "lev"="Z", "level"="Z", "X"="X", "Y"="Y", "Z"="Z", "T"="T")))
+  return(sapply(dim.names, function(x, y) { ifelse(any(tolower(x) == tolower(names(y))), y[tolower(x) == tolower(names(y))], NA) }, c("lat"="Y", "latitude"="Y", "lon"="X", "longitude"="X", "xc"="X", "yc"="Y", "x"="X", "y"="Y", "t"="T", "date"="T", "datetime"="T", "d"="T", "time"="T", "timeofyear"="T", "plev"="Z", "lev"="Z", "level"="Z")))
 }
 
 #' Get a list of dimension variables and axes for a variable's coordinate variable
